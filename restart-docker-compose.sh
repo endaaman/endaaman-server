b07ccdd1bfd1957dea5603323e1ba8ac0e8fe579
endaaman <buhibuhidog@gmail.com>
2020-05-05 02:38:08 +0900

    update

--- restart-docker-compose.sh
+++ restart-docker-compose.sh
@@ -1,4 +1,5 @@
 cd $(realpath $(dirname "$0"))
-docker-compose pull
-docker-compose up -d --build
-echo OK
+
+docker-compose pull > /dev/null && docker-compose up -d --build > /dev/null
+
+echo done
