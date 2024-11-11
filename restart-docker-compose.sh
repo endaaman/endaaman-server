bc1bfa61fdeab558129e54b1a30ad2993bd37e4f
endaaman <ken@endaaman.com>
2024-11-11 16:26:35 +0900

    add .env

--- restart-docker-compose.sh
+++ restart-docker-compose.sh
@@ -1,6 +1,9 @@
 set -eu
 cd $(realpath $(dirname "$0"))
 
+mkdir -p ./docker-volumes/acme-state
+mkdir -p ./docker-volumes/le-certs
+
 docker compose pull
 docker compose up -d --build --quiet-pull
 
